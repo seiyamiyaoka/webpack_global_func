50606ba70786badf0fd25b746d56641a8b039335
宮岡聖也 <miyaokakiyoshinari@miyaokakiyoshinarinoMacBook-Pro-2.local>
2021-03-31 17:36:04 +0900

    場所移動追加

--- src/SizeHandler.js
+++ src/SizeHandler.js
@@ -3,12 +3,27 @@ const SetSizeHandler = (sourceId) => {
   source.addEventListener('change', SizeHandler);
 }
 
+const SetPositionHandler = (sourceId) => {
+  const source = document.getElementById(sourceId);
+  let val = 0;
+  source.addEventListener('click', PositionHandler, val);
+}
+
 
 const SizeHandler = (e) => {
   const targetElement = document.getElementById("displayArea")
   targetElement.style[e.target.id] = e.target.value + "px"
 }
 
+const PositionHandler = (e) => {
+  const targetElement = document.getElementById("displayContent")
+
+  const nextVal = +(e.target.value) + 1
+  targetElement.style[e.target.id] = nextVal + "px"
+  e.target.value = nextVal
+}
+
 export {
-  SetSizeHandler
+  SetSizeHandler,
+  SetPositionHandler
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import {addNum} from './add2'
 import {SetColorHandler} from './ColorHandler'
-import {SetSizeHandler} from './SizeHandler'
+import {SetSizeHandler, SetPositionHandler} from './SizeHandler'
 const addNum1 = ( numOne = 2, numbTwo = 3 ) => {
  return numOne + numbTwo;
 };
 
+const run = () => {
+  SetColorHandler("colors")
+  SetSizeHandler("height")
+  SetSizeHandler("width")
+  SetPositionHandler("top")
+  SetPositionHandler("right")
+  SetPositionHandler("bottom")
+  SetPositionHandler("left")
+}
+
 module.exports = {
-  SetColorHandler,
-  SetSizeHandler,
-  addNum,
-  addNum1
+  run
 }
 // window.callMe = addNum
